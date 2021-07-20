6c3587fb69fe2b31482ec4580341fda463847d08
Hassan <hassan@gmail.com>
2021-07-20 23:38:51 +0500

    7/20

---
+++ Procfile
@@ -0,0 +1,1 @@
+ web: gunicorn insta_bot.wsgi --log-file -
--- metropark/settings.py
+++ metropark/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h90yj(a#f%v1p9sexcsa&0qkz+$=&x#$&o$plkh&d7lly1c0(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['metroparking.herokuapp.com','127.0.0.1']
 
 
 # Application definition
